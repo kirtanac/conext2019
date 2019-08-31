731d6186c3480554acedf2d2c5ca82f03dd9a59e
Kirtana <kirtana@umich.edu>
2019-08-30 22:28:02 -0400

    adding registration site

--- src/registration.js
+++ src/registration.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Card, Container, Button } from 'react-bootstrap';
+
 class Registration extends React.Component {
   render() {
-    return <h1>Registration</h1>
+    return (<div><h1>Registration</h1>
+
+<Button variant="dark" href="https://forms.gle/Gfm4ZkBXAuehoYeAA">Registration</Button>
+
+</div>
+    );
   }
 }
 export default Registration
